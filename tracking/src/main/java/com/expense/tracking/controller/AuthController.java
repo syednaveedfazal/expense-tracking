b99cd6325d94a448a7f3ca64cdc445a6d7389ad7
Syed Naveed Fazal <syednaveedfazal@gmail.com>
2025-04-11 19:00:11 +0530

    email service

--- tracking/src/main/java/com/expense/tracking/controller/AuthController.java
+++ tracking/src/main/java/com/expense/tracking/controller/AuthController.java
@@ -3,6 +3,7 @@ package com.expense.tracking.controller;
 import com.expense.tracking.entities.RefreshToken;
 import com.expense.tracking.model.UserInfoDTO;
 import com.expense.tracking.response.JwtResponseDTO;
+import com.expense.tracking.services.EmailService;
 import com.expense.tracking.services.JwtService;
 import com.expense.tracking.services.RefreshTokenService;
 import com.expense.tracking.services.UserDetailsServiceImpl;
@@ -12,6 +13,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class AuthController {
@@ -24,6 +28,20 @@ public class AuthController {
     @Autowired
     private UserDetailsServiceImpl userDetailsService;
 
+    @Autowired
+    private EmailService emailService;
+
+    public boolean sendEmail(String recipientEmail) {
+        Map<String, Object> model = new HashMap<>();
+        model.put("name", "Syed");
+        model.put("title", "Welcome!");
+        model.put("body", "Thanks for signing up. Enjoy our service!");
+
+        emailService.sendTemplateMail(recipientEmail, "Welcome Email", model);
+
+        return true;
+    }
+
     @PostMapping("/auth/v1/signup")
     public ResponseEntity SignUp(@RequestBody UserInfoDTO userInfoDto) {
         try {
@@ -33,6 +51,7 @@ public class AuthController {
             }
             RefreshToken refreshToken = refreshTokenService.createRefreshToken(userInfoDto.getUserName());
             String jwtToken = jwtService.GenerateToken(userInfoDto.getUserName());
+            sendEmail(userInfoDto.getEmail());//sendTemplateMail(userInfoDto.getEmail());
             return new ResponseEntity<>(JwtResponseDTO.builder().accessToken(jwtToken).
                     token(refreshToken.getToken()).build(), HttpStatus.OK);
         } catch (Exception ex) {
